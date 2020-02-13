1bec5265e6a16e1aabe80e01c315612c3ad9c19f
jyoti.a.patil <jyoti.a.patil@BDC11-L-7200D6V>
2020-02-13 15:52:01 +0530

    my changes

--- FirstDemo/FirstDemo/Controllers/HomeController.cs
+++ FirstDemo/FirstDemo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FirstDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description.";
 
             return View();
         }
